8962470191ac1025dab067c89711704ac277035a
WhiteLeaf <128338163+WhiteLeaf226@users.noreply.github.com>
2023-10-28 12:39:48 +0800

    Update chatlist.h

--- chat_server/chatlist.h
+++ chat_server/chatlist.h
@@ -40,11 +40,17 @@ class ChatInfo
 public:
 	ChatInfo();
 	
+	//判断群是否存在
 	bool info_group_exist(string);
+	//判断用户是否在群内
 	bool info_user_in_group(string, string);
+	//更新群链表
 	void info_group_add_user(string, string);
+	//获取聊天对象的bufferevent
 	struct bufferevent* info_get_friend_dev(string);
+	//获取群成员
 	string info_get_group_member(string);
+	//添加新群
 	void info_add_new_group(string, string);
 		
 	~ChatInfo();
